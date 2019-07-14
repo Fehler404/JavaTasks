9b9cc41ade609bf9e83250f6c43562ded7fe1be4
Fehler404 <52881120+Fehler404@users.noreply.github.com>
2019-07-14 16:35:05 +0300

    Create w3_3.java

---
+++ w3_3.java
@@ -0,0 +1,10 @@
+//Write a Java program to divide two numbers and print on the screen. 
+
+public class Basic {
+
+    public static void main(String[] args) {
+            int result = 25/12;
+            System.out.println(result);
+    }
+    
+}
