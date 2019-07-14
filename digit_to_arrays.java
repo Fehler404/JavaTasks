7b08f13c09122573c26cdf54ae1c780166fb1b2d
Fehler404 <52881120+Fehler404@users.noreply.github.com>
2019-07-14 23:42:48 +0300

    Create digit_to_arrays.java

---
+++ digit_to_arrays.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Convert {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Enter a:");
+        int a = scanner.nextInt();
+
+        List<Integer> digits = new ArrayList<Integer>();
+        List<Integer> remainders = new ArrayList<Integer>();
+
+        while (a > 0) {
+
+            double digit = a % 10;
+            a = a / 10;
+            int whole = (int) digit / 2;
+            int rem = (int) digit % 2;
+
+            digits.add(whole);
+            remainders.add(rem);
+        }
+
+        Collections.reverse(digits);
+        Collections.reverse(remainders);
+
+        for (int i = 0; i < digits.size() ; i++) {
+
+            System.out.println(digits.get(i) * Math.pow(2, i));
+
+        }
+
+//        for (int i = 0; i < digits.size(); i++) {
+//
+//            System.out.println(digits.get(i));
+//
+//        }
+//
+//        for (int i = 0; i < remainders.size(); i++) {
+//
+//            System.out.println(remainders.get(i));
+//
+//        }
+
+        System.out.println("Whole part of division: " + Arrays.deepToString(digits.toArray()));
+        System.out.println("Remainders: " + Arrays.deepToString(remainders.toArray()));
+    }
+}
+
+
